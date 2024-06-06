c2d7682684502c95eddea4a273a15e18c5105f47
Francisco <fjmorosv@gmail.com>
2024-06-06 18:16:36 -0400

    Arregle el boton de JS

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const boton = document.querySelector('.button');
 const container = document.querySelector('.container');
 
-boton.addEventListener('click',function(){
-    const tituloAyuda = document.createElement('h2');
-    tituloAyuda.innerHTML += "tu Ayuda";
-    tituloAyuda.style.textAlign = "center";
-    tituloAyuda.style.justifyContent = "center";
-    container.appendChild(tituloAyuda);
+let click = false;
+boton.addEventListener('click', function () {
+    if (click == false) {
+        const tituloAyuda = document.createElement('h2');
+        tituloAyuda.innerHTML += "tu Ayuda";
+        tituloAyuda.style.textAlign = "center";
+        tituloAyuda.style.justifyContent = "center";
+        container.appendChild(tituloAyuda);
+        click = true;
+    }
 });
